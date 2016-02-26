9e0796512a6699b10d6d1eea3839dbe7d5030e22
Filip Stankovic <filips@gmail.com>
2016-02-26 10:23:57 -0500

    Generate static welcome page and say Hello World

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#welcome'
+
+  get 'static_pages/welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
