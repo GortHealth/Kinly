128ba7478062a496060b48c20463ab667d8963da
Filip Stankovic <filips@gmail.com>
2016-02-27 10:50:52 -0500

    Setup user and session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-
-  get 'user_sessions/create'
-
-  get 'user_sessions/destroy'
-
+  root :to => 'users#index'
+  resources :user_sessions
   resources :users
-  root 'static_pages#welcome'
 
-  get 'static_pages/welcome'
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
