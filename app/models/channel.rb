f76423ff450aa26acf79b26357c9aeb30b14ba78
Filip Stankovic <filips@gmail.com>
2016-02-27 16:20:37 -0500

    Fix bug in logic of setting channel numbers when changing active status

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,7 +3,7 @@ class Channel < ActiveRecord::Base
   belongs_to :target
 
   after_create :set_defaults
-  before_save :make_channel_inactive
+  before_save :set_channel_number
 
   def set_defaults
     self.number ||= -1 # Channel.maximum('number') + 1 This needs to be set when channel is made active somewhere
@@ -11,26 +11,20 @@ class Channel < ActiveRecord::Base
     self.save
   end
 
-  def make_channel_inactive
+  def set_channel_number
     if self.active == false
       self.number = -1
     elsif self.active == true && self.number == -1
       max = Channel.maximum("number") + 1
       counter = 1
-      Channel.select.("number").each do |num|
-        if num != counter
+      max.times do
+        # byebug
+        if Channel.exists?(:number => counter)
+          counter += 1
+        else
           self.number = counter
           return
-        else
-          counter += 1
         end
-      # max.times do
-      #   byebug
-      #   if Channel.select("number") != counter
-      #     self.number = counter
-      #     return
-      #   end
-      #   counter += 1
       end
     end
   end
