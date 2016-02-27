dcbf82e8d099d0575203845e64ec9970e73ba12b
Filip Stankovic <filips@gmail.com>
2016-02-27 12:55:01 -0500

    Setup photos controller with basic actions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,34 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to photos_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_path
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:name, :url, :album_id, :user_id)
+  end
 end
