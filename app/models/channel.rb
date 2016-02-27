96db269451cdbc58d0745fda271ccbc3b617ab4a
Filip Stankovic <filips@gmail.com>
2016-02-27 13:02:21 -0500

    Add basic logic to channel model for setting default variables

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,12 @@
 class Channel < ActiveRecord::Base
   has_and_belongs_to_many :photos
   belongs_to :target
+
+  after_create :set_defaults
+
+  def set_defaults
+    self.number ||= -1 # Channel.maximum('number') + 1 This needs to be set when channel is made active somewhere
+    self.active ||= false
+    self.save
+  end
 end
