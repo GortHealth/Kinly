30600a5263c1e2d1c4902c7aec36ef51395e0f39
Filip Stankovic <filips@gmail.com>
2016-02-27 12:53:40 -0500

    Setup targets controller with basic actions

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,2 +1,44 @@
 class TargetsController < ApplicationController
+  def create
+    @target = Target.new(target_params)
+
+    if @target.save
+      @target.auth = SecureRandom.hex(16)
+      redirect_to target_path(@target)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @target = Target.new
+  end
+
+  def edit
+    @target = Target.find(params[:id])
+  end
+
+  def show
+    @target = Target.find(params[:id])
+  end
+
+  def update
+    @target = Target.find(params[:id])
+    if @target.update_attributes(target_params)
+      redirect_to target_path(@target)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @target = Target.find(params[:id])
+    @target.destroy
+    redirect_to new_target_path
+  end
+
+  private
+  def target_params
+    params.require(:target).permit(:name, :last_name)
+  end
 end
