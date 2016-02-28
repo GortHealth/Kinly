7e3d97364c6f49243e4d7c8021513e84d0dc4b19
Chris Staikos <cstaikos@gmail.com>
2016-02-28 01:18:07 -0500

    Map 'u' to channel up and 'd' to channel down via jquery for changing channels via ir remote

--- app/assets/javascripts/targets.js
+++ app/assets/javascripts/targets.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('keypress', function(e) {
+  href = window.location.href;
+  baseUrl = href.substr(0, href.lastIndexOf('/'))
+  currentChannel = href.substr(href.lastIndexOf('/') + 1);
+  if (e.which === 117) { // 'u' for up
+    newChannel = parseInt(currentChannel) + 1;
+  }
+  else { //'d' for down
+    newChannel = parseInt(currentChannel) - 1;
+  }
+
+  window.location.href = baseUrl + '/' + newChannel;
+});
